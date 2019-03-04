a1445776d1b147b0faf7402b4b551246eab61718
Samuel W Tong <ssamt1142@gmail.com>
2019-03-04 04:30:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest
+shipping_manifest["mus"]
 
   # add 4 gun powder to the shipping_manifest hash below
 
